316f5f16fb1f78226524bd0e580521cf666b541e
Jesse <jelloeater@gmail.com>
2019-01-29 21:39:50 -0500

    Started base framework Added logging Started on arg parse

---
+++ libs/output.go
@@ -0,0 +1,7 @@
+package libs
+import log "github.com/sirupsen/logrus"
+
+func GenerateXML()  {
+	log.Debug("Start XML generation")
+	println("SOME XML")
+}
--- main.go
+++ main.go
@@ -1,1 +1,105 @@
-package UptimeParserGo
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+	"os"
+	"./libs"
+	"fmt"
+)
+
+func main() {
+	log.SetReportCaller(true)
+
+	app := cli.NewApp()
+	app.Name = "UptimeParserGo"
+	app.Usage = ""
+	app.HideVersion = true
+	app.HideHelp = false
+	app.EnableBashCompletion = true
+
+	// Setup flags here
+	var DebugMode bool
+	var IpAddress string
+	var Snmp string
+	flags := []cli.Flag{
+		cli.BoolFlag{
+
+			Name:        "debug, d",
+			Usage:       "enable debug mode",
+			Destination: &DebugMode,
+		},
+		cli.StringFlag{
+
+			Name:        "ip, i",
+			Usage:       "IP address to scan",
+			Destination: &IpAddress,
+		},
+		cli.StringFlag{
+
+			Name:        "snmp, s",
+			Usage:       "SNMP community string",
+			Destination: &Snmp,
+		},
+	}
+
+	// Commands to be run go here, after parsing variables
+	app.Commands = []cli.Command{
+		{
+			UseShortOptionHandling: true,
+			Name:    "xml",
+			Aliases: []string{"x"},
+			Usage:   "export as XML",
+			Category: "output",
+			Action: func(c *cli.Context) error {
+				args := c.Args() // Converts first arg from string to int
+				log.Info(args)
+
+				libs.GenerateXML()
+
+				return nil
+			},
+		},
+		{
+			UseShortOptionHandling: true,
+			Name:    "json",
+			Aliases: []string{"j"},
+			Usage:   "export as JSON",
+			Category: "output",
+			Action: func(c *cli.Context) error {
+				args := c.Args() // Converts first arg from string to int
+				log.Info(args)
+
+				//DO WORK HERE
+				return nil
+			},
+		},
+	}
+
+	app.Flags = flags // Assign flags via parse right before we start work
+	app.Before = func(c *cli.Context) error {
+		// Actions to run before running parsed commands
+		if DebugMode {
+			log.SetLevel(5)
+			log.Info("Debug Mode")
+		} else {
+			log.SetLevel(3)
+			// open a file
+			f, err := os.OpenFile("uptime.log", os.O_CREATE | os.O_RDWR, 0666) // Create new log file every run
+			//f, err := os.OpenFile("uptime.log", os.O_APPEND | os.O_CREATE | os.O_RDWR, 0666)
+			if err != nil {
+				fmt.Printf("error opening file: %v", err)
+			}
+			log.SetOutput(f)
+			log.Warn("Normal Mode")
+		}
+		return nil
+	}
+	// Parse Commands and flags here
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Info("EOP")
+}
